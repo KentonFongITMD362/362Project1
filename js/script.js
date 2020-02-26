0a4752fe212f4d104edbd56b3f79e0ca38f09e4d
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2020-02-25 22:49:47 -0600

    adding radio submit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ function form() {
   var fullname = document.getElementById("fullname").value;
   var email = document.getElementById("email").value;
   var address = document.getElementById("address").value;
-  
+  var money =document.querySelector('input[name="money"]:checked').value;
   
 }
