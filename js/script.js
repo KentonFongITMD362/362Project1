c77414c8cb56b84086398c567d3edd5016c51bdf
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2020-02-25 23:10:00 -0600

    testing js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-function form() {
+function Form() {
   var fullname = document.getElementById("fullname").value;
   var email = document.getElementById("email").value;
   var address = document.getElementById("address").value;
-  //var money = document.querySelector('input[name="money"]:checked').value;
+  var money = document.querySelector('input[name="money"]:checked').value;
   document.getElementById("infoform").submit();
-  alert(fullname + email + address + money);
+  //alert(fullname + email + address + money);
+  alert("testing");
+  return true;
 }
