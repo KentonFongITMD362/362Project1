5c8fd0c7b41f0d9a8fba76983a3f7e94dede44c0
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2020-02-25 22:46:37 -0600

    adding email and address

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 function form() {
   var fullname = document.getElementById("fullname").value;
+  var email = document.getElementById("email").value;
+  var address = document.getElementById("address").value;
+  
   
 }
